dc6870a8edebd6c4fe3da2fa2060b38bba83068a
Leafony <52395367+Leafony@users.noreply.github.com>
2022-12-19 18:58:23 +0900

    Add files via upload

---
+++ avr/Blink_Pjt/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+//=====================================================================
+//  Blink
+//
+//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
+//    Released under the MIT license
+//    https://opensource.org/licenses/MIT
+//
+//      Rev.00 2021/04/01  First release
+//=====================================================================
+
+void setup() {
+  // LEDピンを出力ピンに設定
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // LEDを点灯
+  delay(1000);                       // 1秒待つ
+  digitalWrite(LED_BUILTIN, LOW);    // LEDを消灯
+  delay(1000);                       // 1秒待つ
+}
