ab46dcc914dfb3f10c95b783b5c02c5a4215490b
hayagui <121139174+hayagui@users.noreply.github.com>
2022-12-25 01:42:59 +0900

    Add files via upload

--- avr/toneMelody_Pjt/src/main.cpp
+++ avr/toneMelody_Pjt/src/main.cpp
@@ -37,7 +37,7 @@ void setup() {
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
     // tone(8, melody[thisNote], noteDuration); // original
-    tone(8, melody[thisNote], noteDuration);   // Leafony 
+    tone(5, melody[thisNote], noteDuration);   // Leafony 
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
