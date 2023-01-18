b52e4a5671416734aeacb55597e13bdafb7c7e77
Leafony <52395367+Leafony@users.noreply.github.com>
2023-01-18 10:22:00 +0900

    Update main.cpp

--- avr/Accelerometer_Pjt/src/main.cpp
+++ avr/Accelerometer_Pjt/src/main.cpp
@@ -1,14 +1,12 @@
-#include <Arduino.h>
-
 //=====================================================================
-//  Accelerometer
-//
-//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
+//    (c) 2023 LEAFONY SYSTEMS Co., Ltd
 //    Released under the MIT license
 //    https://opensource.org/licenses/MIT
 //
-//      Rev.00 2021/04/01  First release
+//      Rev.00 2023/02/01  First release
 //=====================================================================
+#include <Arduino.h>
+
 #include <Adafruit_LIS3DH.h>
 
 #define LIS3DH_ADDRESS 0x19
