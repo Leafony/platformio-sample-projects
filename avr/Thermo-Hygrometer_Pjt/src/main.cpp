ffba6fb7c5dd90eb89e6c118ea165f0a86e80241
Leafony <52395367+Leafony@users.noreply.github.com>
2023-01-18 10:30:16 +0900

    Update main.cpp

--- avr/Thermo-Hygrometer_Pjt/src/main.cpp
+++ avr/Thermo-Hygrometer_Pjt/src/main.cpp
@@ -1,15 +1,14 @@
-#include <Arduino.h>
-
 //=====================================================================
-//  Thermo-Hygrometer
-//
-//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
+//    (c) 2023 LEAFONY SYSTEMS Co., Ltd
 //    Released under the MIT license
 //    https://opensource.org/licenses/MIT
 //
-//      Rev.00 2021/04/01  First release
+//      Rev.00 2023/02/01  First release
 //=====================================================================
+#include <Arduino.h>
+
 #include <Wire.h>
+
 #include <HTS221.h>
 
 //---------------------------
