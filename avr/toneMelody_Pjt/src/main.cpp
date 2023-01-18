31aa8893cceb3f210a3bd08339c934e55f391762
Leafony <52395367+Leafony@users.noreply.github.com>
2023-01-18 10:30:53 +0900

    Update main.cpp

--- avr/toneMelody_Pjt/src/main.cpp
+++ avr/toneMelody_Pjt/src/main.cpp
@@ -1,3 +1,10 @@
+//=====================================================================
+//    (c) 2023 LEAFONY SYSTEMS Co., Ltd
+//    Released under the MIT license
+//    https://opensource.org/licenses/MIT
+//
+//      Rev.00 2023/02/01  First release
+//=====================================================================
 #include <Arduino.h>
 
 /*
