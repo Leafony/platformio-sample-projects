5da1a0132fe2e21703370a635442a2c5518dbc72
Leafony <52395367+Leafony@users.noreply.github.com>
2023-01-25 13:57:43 +0900

    Update main.cpp

--- avr/Blink_Pjt/src/main.cpp
+++ avr/Blink_Pjt/src/main.cpp
@@ -8,11 +8,13 @@
 #include <Arduino.h>
 
 void setup() {
-  Serial.begin(115200);
-  delay(10);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop(){
-  Serial.println("Hello World");
-  delay(1000);
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
