ce97a10bd5515cda09d0358644ebeba3aea8b9f7
tokihikomori <119828392+tokihikomori@users.noreply.github.com>
2024-02-28 14:53:23 +0900

    Add files via upload

---
+++ stm32/STM32_GoogleSheetsLogger/README.md
@@ -0,0 +1,12 @@
+# Google Sheets Logger
+
+## 動作手順
+
+1. 下記の記述に従い Google App Script を設定した Google Sheets を用意する
+https://docs.leafony.com/docs/examples/advanced/2_p/esp32/esp32_googlesheets_1/#google-apps-script%E3%81%AE%E8%A8%AD%E5%AE%9A
+
+2. `arduino_secrets.h` に、1で作成したシートのシートIDと、Wi-Fiの接続情報を入力
+
+3. 4-Sensorsリーフ、STM32リーフ、USBリーフ、WiFiリーフを接続
+
+4. スケッチを書き込み
---
+++ stm32/STM32_GoogleSheetsLogger/src/arduino_secrets.h
@@ -0,0 +1,18 @@
+// デバイス名
+#define DEVICE_NAME "Leafony_A"
+
+// 送信間隔
+#define INTERVAL 600
+
+// デバッグメッセージ (コメントアウトで無効化)
+#define DEBUG
+
+// Wi-Fi
+#define SECRET_SSID "IODATA-5dae7e-2G"
+#define SECRET_PASS "fd8na48807289"
+
+// スリープ時の処理 (DOZE_MODE: Dozeモードで待機, コメントアウト: Shutdownモードで待機)
+//#define DOZE_MODE
+
+// Google App Script
+#define GAS_DEPLOY_ID "AKfycbwxtJZQbYOo1C7xsr3rovkHi3zk88iETftLlF5cigDxe3mgQyWZ7UycBpECIuUMhL9R"
